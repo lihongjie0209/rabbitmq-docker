87ea80f9cabfb6b8aa815eaf73d0f2a155e5e58b
lihongjie0209 <Qy8NzPH*4KJX>
2020-08-13 21:35:36 +0800

    晚上集群配置

--- cluster/docker-entrypoint.sh
+++ cluster/docker-entrypoint.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/bin/sh
+echo "start config cookie"
 mkdir -p /var/lib/rabbitmq/
+echo "mkdir -p /var/lib/rabbitmq/"
 echo "123456789" > /var/lib/rabbitmq/.erlang.cookie
 
 chmod 600 /var/lib/rabbitmq/.erlang.cookie
 
-echo /var/lib/rabbitmq/.erlang.cookie
+cat /var/lib/rabbitmq/.erlang.cookie
 exec "$@"
